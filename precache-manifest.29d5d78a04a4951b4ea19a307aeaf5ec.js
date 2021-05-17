80ab604dbd8591182f6cc515c4459806610ff3b7
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-17 15:05:20 +0300

    Updates

---
+++ precache-manifest.29d5d78a04a4951b4ea19a307aeaf5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794d8783f22f610a9b2a22e0d28e7f33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84c17b3a325a33f9d69d",
+    "url": "./static/css/main.90522076.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c17b3a325a33f9d69d",
+    "url": "./static/js/main.e4a94fc8.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
